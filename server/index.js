10f02b16ab4eb67216e11083513a1139c32bf103
Kaviha R. M <Kaviharavichandran2006@gmail.com>
2025-07-28 22:32:21 +0530

    Added dotenv to fix deployment error

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-import dotenv from "dotenv";
+// Load .env variables
+import dotenv from 'dotenv';
 dotenv.config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./auth');
-const orderRoutes = require('./order');
+// Import dependencies
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// Import routes
+import authRoutes from './auth.js';
+import orderRoutes from './order.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
